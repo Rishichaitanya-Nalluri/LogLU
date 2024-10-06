9ede1ce08f7e2362d3408ca7ff153d5e9011a9fb
N. Rishi Chaitanya Sri Prasad <94001904+Rishichaitanya-Nalluri@users.noreply.github.com>
2024-10-06 14:56:40 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 from setuptools import setup, find_packages
+
 setup(
     name='loglu',  
     version='0.1.0',
     description='The Logarithmic Linear Unit (LogLU) is a novel activation function designed for deep neural networks, improving convergence speed, stability, and overall model performance',
-    long_description=open('README.md').read() + "\n\n## Patent Notice\n\nThis software implements methods covered by Indian Published Patent Application No. 202341087256, currently published but not yet granted. Use of the patented methods may require a separate license. Please refer to the LICENSE file for more details.",
+    long_description=open('README.md', encoding='utf-8').read() + "\n\n## Patent Notice\n\nThis software implements methods covered by Indian Published Patent Application No. 202341087256, currently published but not yet granted. Use of the patented methods may require a separate license. Please refer to the LICENSE file for more details.",
     long_description_content_type='text/markdown',
     author='Rishi Chaitanya Sri Prasad Nalluri',
     author_email='rishichaitanya888@gmail.com',
